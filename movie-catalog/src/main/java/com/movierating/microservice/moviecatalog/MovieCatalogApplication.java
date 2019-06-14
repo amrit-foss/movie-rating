4cba1dc4952b95acbd876641679c56472d8faccc
Amrit <[amritpapani@gmail.com]>
2019-06-14 20:59:36 +0530

    Launcher file and maven dependencies

---
+++ movie-catalog/src/main/java/com/movierating/microservice/moviecatalog/MovieCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.movierating.microservice.moviecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogApplication.class, args);
+	}
+
+}
