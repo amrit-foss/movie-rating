61185d2fe278ee5dbe19c2ae45db82604b201f5c
Amrit <[amritpapani@gmail.com]>
2019-06-15 00:03:04 +0530

    Initial commit of the movie ratings service

---
+++ movie-rating/src/main/java/com/movierating/microservice/movierating/MovieRatingApplication.java
@@ -0,0 +1,13 @@
+package com.movierating.microservice.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingApplication.class, args);
+	}
+
+}
