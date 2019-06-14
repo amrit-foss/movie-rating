1a57e15b426e77b9339f897849a2fbdfcd6aec4e
Amrit <[amritpapani@gmail.com]>
2019-06-14 20:58:47 +0530

    Launcher file

---
+++ movie-catalog/src/test/java/com/movierating/microservice/moviecatalog/MovieCatalogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.movierating.microservice.moviecatalog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieCatalogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
