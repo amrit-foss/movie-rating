f34ce0cc4bd23fcc4f5eb64295e32ea01ecf37aa
Amrit <[amritpapani@gmail.com]>
2019-06-16 00:33:22 +0530

    First Rest Api call from movie catalog service to movie info service

---
+++ movie-info/src/test/java/com/movierating/microservice/movieinfo/MovieInfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.movierating.microservice.movieinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieInfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
